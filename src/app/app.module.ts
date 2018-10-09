749fd069e17cc1b0fabbb8bd3b71cd6d4f1df631
stanicchris <chris574topher@gmail.com>
2018-10-09 12:02:50 +0200

    create pizzas-list

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,19 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+import { HttpClientModule }    from '@angular/common/http';
 import { AppComponent } from './app.component';
+import { PizzaComponent } from './pizza/pizza.component';
+import { PizzaListComponent } from './pizza-list/pizza-list.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PizzaComponent,
+    PizzaListComponent
   ],
   imports: [
+    HttpClientModule,
     BrowserModule
   ],
   providers: [],
