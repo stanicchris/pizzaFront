78b480dafaca45475b757c2d865f2765bb3ba880
stanicchris <chris574topher@gmail.com>
2018-10-23 10:37:27 +0200

    lambda

--- function/hello.js
+++ function/hello.js
@@ -1,4 +1,4 @@
-//const request = require('request');
+const request = require('request');
 
 exports.handler = (event, context, callback) => {
     console.log('Received event:', JSON.stringify(event, null, 2));
