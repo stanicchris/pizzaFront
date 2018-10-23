16351a67e47be735f871ef7725a800c0a8e3a973
stanicchris <chris574topher@gmail.com>
2018-10-23 11:06:31 +0200

    lambda

--- function/yes.js
+++ function/yes.js
@@ -1,10 +1,29 @@
+const request = require('request');
+
 exports.handler = (event, context, callback) => {
     console.log('Received event:', JSON.stringify(event, null, 2));
 
     //Return 200 to caller
     callback(null, {
-        statusCode: '200',
-        body:""
+        statusCode: '200'
     });
 
+    //Read the IPN message sent from PayPal and prepend 'cmd=_notify-validate'
+    var body = 'cmd=_notify-validate&' + event.body;
+
+    console.log('Verifying');
+    console.log(body);
+
+    var options = {
+        url: 'https://ipnpb.sandbox.paypal.com/cgi-bin/webscr',
+        method: 'POST',
+        headers: {
+            'Connection': 'close'
+        },
+        body: body,
+        strictSSL: true,
+        rejectUnauthorized: false,
+        requestCert: true,
+        agent: false
+    };
 };
