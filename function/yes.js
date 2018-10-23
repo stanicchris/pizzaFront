934772a6b5ff29b4a05295460631b8ce7cf5e0bb
stanicchris <chris574topher@gmail.com>
2018-10-23 11:39:22 +0200

    lambda

--- function/yes.js
+++ function/yes.js
@@ -36,7 +36,6 @@ export function handler(event, context, callback) {
 
                 //The IPN is verified
                 console.log('Verified IPN!');
-                console.log(JSON.stringify(JSON.parse(body), null, 2));
             } else if (body.substring(0, 7) === 'INVALID') {
 
                 //The IPN invalid
