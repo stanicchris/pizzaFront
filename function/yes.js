ace56aa8b7e893b299487fb896366310e9a926d2
stanicchris <chris574topher@gmail.com>
2018-10-23 11:10:37 +0200

    lambda

--- function/yes.js
+++ function/yes.js
@@ -1,11 +1,16 @@
-const request = require('request');
+/**
+ * Created by chrissewell on 31/10/2016.
+ */
 
-exports.handler = (event, context, callback) => {
+import request from 'request';
+
+export function handler(event, context, callback) {
     console.log('Received event:', JSON.stringify(event, null, 2));
 
     //Return 200 to caller
     callback(null, {
         statusCode: '200'
+        
     });
 
     //Read the IPN message sent from PayPal and prepend 'cmd=_notify-validate'
@@ -26,4 +31,31 @@ exports.handler = (event, context, callback) => {
         requestCert: true,
         agent: false
     };
-};
+
+    //POST IPN data back to PayPal to validate
+    request(options, function callback(error, response, body) {
+        if (!error && response.statusCode === 200) {
+
+            //Inspect IPN validation result and act accordingly
+            if (body.substring(0, 8) === 'VERIFIED') {
+
+                //The IPN is verified
+                console.log('Verified IPN!');
+            } else if (body.substring(0, 7) === 'INVALID') {
+
+                //The IPN invalid
+                console.log('Invalid IPN!');
+            } else {
+                //Unexpected response body
+                console.log('Unexpected response body!');
+                console.log(body);
+            }
+        }else{
+            //Unexpected response
+            console.log('Unexpected response!');
+            console.log(response);
+        }
+
+    });
+
+}
